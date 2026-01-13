f929da387ad2a36cb43551578707afd37c27dad2
zeahmd <zeeshanahmad10809@gmail.com>
2026-01-14 00:13:30 +0100

    updated configs. even though they'll change during experiments, just trying to save for future reference.

--- generate_img.py
+++ generate_img.py
@@ -6,7 +6,15 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 generator = torch.Generator(device=device).manual_seed(42)
 
 sana = SanaPipeline("configs/sana1-5_config/1024ms/Sana_1600M_1024px_allqknorm_bf16_lr2e5.yaml")
-checkpoint = torch.load("/home/woody/vlgm/vlgm116v/output/debug/checkpoints/epoch_17_step_5000.pth", map_location=device, weights_only=False)
+# the following from_pretrained loads checkpoints but contains only 1 dict with key 'state_dict'
+# these weights are only for the SANA model, not the entire pipeline (such as DC-AE and Gemma-2)
+# sana.from_pretrained("/home/hpc/vlgm/vlgm116v/.cache/huggingface/hub/models--Efficient-Large-Model--SANA1.5_1.6B_1024px/snapshots/a7b7e39161522e12d98045d2c1b5da5101d712f8/checkpoints/SANA1.5_1.6B_1024px.pth")
+
+# these training checkpoints contain the many dicts including 'state_dict' for SANA model, 'optimizer', 'lr_scheduler', etc.
+# we only need 'state_dict' for inference, so we load that into the sana model
+# Sanity Check: set(new_checkpoint['state_dict'].keys()) - set(sana.state_dict().keys()) should contain only
+# the CrossStitchBlock parameters if those were newly added
+checkpoint = torch.load("/home/woody/vlgm/vlgm116v/output/debug/checkpoints/epoch_1_step_10.pth", map_location=device, weights_only=False)
 sana.load_state_dict(checkpoint["state_dict"], strict=False)
 sana.to(device)
 sana.eval()
