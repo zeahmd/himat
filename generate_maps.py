41ff68b60dd019cd923b6494aed2e24c143cf268
zeahmd <zeeshanahmad10809@gmail.com>
2026-01-14 00:12:12 +0100

    created svbrdf generation script and also changed the image generation script.

--- generate_maps.py
+++ generate_maps.py
@@ -12,19 +12,15 @@ sana.to(device)
 sana.eval()
 
 
-
-prompt = 'a cyberpunk cat with a neon sign that says "Sana"'
-while True:
-    prompt = input("Enter your prompt: ")  # Get user input for the prompt
-    img_name = input("Enter the output image name (with .png extension): ")  # Get user input for the image name
+prompts = ['a surface with white and black checkered pattern'] * 3
     
-    image = sana(
-        prompt=prompt,
-        height=1024,
-        width=1024,
-        guidance_scale=4.5,
-        pag_guidance_scale=1.0,
-        num_inference_steps=20,
-        generator=generator,
-    )
-    save_image(image, f'output/{img_name}', nrow=1, normalize=True, value_range=(-1, 1))
+image = sana(
+    prompt=prompts,
+    height=1024,
+    width=1024,
+    guidance_scale=4.5,
+    pag_guidance_scale=1.0,
+    num_inference_steps=20,
+    generator=generator,
+)
+save_image(image[0], f'output/tex_map_basecolor.png', nrow=1, normalize=True, value_range=(-1, 1))
